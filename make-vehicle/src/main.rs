e383bf3a2cc10c07133ca479ca1d31dc30b47e4e
mix <mixiate@proton.me>
2025-12-11 04:21:44 +0000

    make-vehicle: Try unwrap image dimensions when creating ride object

--- make-vehicle/src/main.rs
+++ make-vehicle/src/main.rs
@@ -693,8 +693,8 @@ fn render(
                 y: image.offset.y,
                 src_x: Some(coord.x),
                 src_y: Some(coord.y),
-                src_width: Some(image.width as i32),
-                src_height: Some(image.height as i32),
+                src_width: Some(image.width.try_into().unwrap()),
+                src_height: Some(image.height.try_into().unwrap()),
                 format: None,
                 palette: ride_object::ImagePaletteType::Keep,
             });
