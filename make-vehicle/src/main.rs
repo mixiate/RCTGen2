11edf40f7d63791b30ed7ca6c295079348eceffb
mix <mixiate@proton.me>
2025-12-14 06:25:24 +0000

    make-vehicle: Use clap for cli commands

--- make-vehicle/src/main.rs
+++ make-vehicle/src/main.rs
@@ -1,13 +1,16 @@
+#[derive(clap::Parser)]
+struct Cli {
+    ride_description_file_path: std::path::PathBuf,
+}
+
 fn main() -> anyhow::Result<()> {
-    use anyhow::Context as _;
+    use clap::Parser as _;
 
     let start_time = std::time::Instant::now();
 
-    let args: Vec<String> = std::env::args().collect();
+    let cli = Cli::parse();
 
-    let ride_description_path = std::path::PathBuf::from(args.get(1).context("No description file path argument")?);
-
-    make_vehicle::make_vehicle(&ride_description_path)?;
+    make_vehicle::make_vehicle(&cli.ride_description_file_path)?;
 
     println!("Time taken: {} seconds", start_time.elapsed().as_secs_f32());
 
