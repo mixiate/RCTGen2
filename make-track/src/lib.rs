9909dfe0752ff755b5b931d4f2e3fd0560116c84
mix <mixiate@proton.me>
2025-12-26 07:40:26 +0000

    make-track: Load track models

--- make-track/src/lib.rs
+++ make-track/src/lib.rs
@@ -1,7 +1,22 @@
 mod track_desc;
 
 pub fn make_track(track_description_file_path: &std::path::Path) -> anyhow::Result<()> {
+    use anyhow::Context as _;
+
     let desc = track_desc::Desc::load(track_description_file_path)?;
     println!("{desc:?}");
+
+    let base_directory = track_description_file_path.parent().with_context(|| {
+        format!(
+            "Could not get parent directory of {}",
+            track_description_file_path.display()
+        )
+    })?;
+
+    for track in desc.tracks {
+        let models = track.models.load(base_directory)?;
+        println!("{} {}", models.track.meshes.len(), models.mask.meshes.len());
+    }
+
     Ok(())
 }
--- make-track/src/track_desc.rs
+++ make-track/src/track_desc.rs
@@ -63,61 +63,72 @@ fn float_1() -> f32 {
 }
 
 #[derive(Debug, serde::Deserialize)]
-pub struct Models {
-    #[expect(unused)]
-    track: std::path::PathBuf,
-    #[expect(unused)]
-    mask: std::path::PathBuf,
-    #[expect(unused)]
-    tie: Option<std::path::PathBuf>,
-    #[expect(unused)]
-    track_tie: Option<std::path::PathBuf>,
-    #[expect(unused)]
-    track_alt: Option<std::path::PathBuf>,
-    #[expect(unused)]
-    support_flat: Option<std::path::PathBuf>,
-    #[expect(unused)]
-    support_bank_sixth: Option<std::path::PathBuf>,
-    #[expect(unused)]
-    support_bank_third: Option<std::path::PathBuf>,
-    #[expect(unused)]
-    support_bank_half: Option<std::path::PathBuf>,
-    #[expect(unused)]
-    support_bank_two_thirds: Option<std::path::PathBuf>,
-    #[expect(unused)]
-    support_bank_five_sixths: Option<std::path::PathBuf>,
-    #[expect(unused)]
-    support_bank: Option<std::path::PathBuf>,
-    #[expect(unused)]
-    support_base: Option<std::path::PathBuf>,
-    #[expect(unused)]
-    brake: Option<std::path::PathBuf>,
-    #[expect(unused)]
-    block_brake: Option<std::path::PathBuf>,
-    #[expect(unused)]
-    booster: Option<std::path::PathBuf>,
-    #[expect(unused)]
-    magnetic_brake: Option<std::path::PathBuf>,
-    #[expect(unused)]
-    support_steep_to_vertical: Option<std::path::PathBuf>,
-    #[expect(unused)]
-    support_vertical_to_steep: Option<std::path::PathBuf>,
-    #[expect(unused)]
-    support_vertical: Option<std::path::PathBuf>,
-    #[expect(unused)]
-    support_vertical_twist: Option<std::path::PathBuf>,
-    #[expect(unused)]
-    support_barrel_roll: Option<std::path::PathBuf>,
-    #[expect(unused)]
-    support_half_loop: Option<std::path::PathBuf>,
-    #[expect(unused)]
-    support_quarter_loop: Option<std::path::PathBuf>,
-    #[expect(unused)]
-    support_corkscrew: Option<std::path::PathBuf>,
-    #[expect(unused)]
-    support_zero_g_roll: Option<std::path::PathBuf>,
-    #[expect(unused)]
-    support_large_zero_g_roll: Option<std::path::PathBuf>,
+pub struct Models<T> {
+    pub track: T,
+    pub mask: T,
+    pub tie: Option<T>,
+    pub track_tie: Option<T>,
+    pub track_alt: Option<T>,
+    pub support_flat: Option<T>,
+    pub support_bank_sixth: Option<T>,
+    pub support_bank_third: Option<T>,
+    pub support_bank_half: Option<T>,
+    pub support_bank_two_thirds: Option<T>,
+    pub support_bank_five_sixths: Option<T>,
+    pub support_bank: Option<T>,
+    pub support_base: Option<T>,
+    pub brake: Option<T>,
+    pub block_brake: Option<T>,
+    pub booster: Option<T>,
+    pub magnetic_brake: Option<T>,
+    pub support_steep_to_vertical: Option<T>,
+    pub support_vertical_to_steep: Option<T>,
+    pub support_vertical: Option<T>,
+    pub support_vertical_twist: Option<T>,
+    pub support_barrel_roll: Option<T>,
+    pub support_half_loop: Option<T>,
+    pub support_quarter_loop: Option<T>,
+    pub support_corkscrew: Option<T>,
+    pub support_zero_g_roll: Option<T>,
+    pub support_large_zero_g_roll: Option<T>,
+}
+
+impl Models<std::path::PathBuf> {
+    pub fn load(&self, base_directory: &std::path::Path) -> anyhow::Result<Models<renderer::model::Model>> {
+        let load_optional_model = |path: &Option<std::path::PathBuf>| {
+            path.as_ref().map(|x| renderer::model::Model::load(&base_directory.join(x))).transpose()
+        };
+
+        Ok(Models::<renderer::model::Model> {
+            track: renderer::model::Model::load(&base_directory.join(&self.track))?,
+            mask: renderer::model::Model::load(&base_directory.join(&self.mask))?,
+            tie: load_optional_model(&self.tie)?,
+            track_tie: load_optional_model(&self.track_tie)?,
+            track_alt: load_optional_model(&self.track_alt)?,
+            support_flat: load_optional_model(&self.support_flat)?,
+            support_bank_sixth: load_optional_model(&self.support_bank_sixth)?,
+            support_bank_third: load_optional_model(&self.support_bank_third)?,
+            support_bank_half: load_optional_model(&self.support_bank_half)?,
+            support_bank_two_thirds: load_optional_model(&self.support_bank_two_thirds)?,
+            support_bank_five_sixths: load_optional_model(&self.support_bank_five_sixths)?,
+            support_bank: load_optional_model(&self.support_bank)?,
+            support_base: load_optional_model(&self.support_base)?,
+            brake: load_optional_model(&self.brake)?,
+            block_brake: load_optional_model(&self.block_brake)?,
+            booster: load_optional_model(&self.booster)?,
+            magnetic_brake: load_optional_model(&self.magnetic_brake)?,
+            support_steep_to_vertical: load_optional_model(&self.support_steep_to_vertical)?,
+            support_vertical_to_steep: load_optional_model(&self.support_vertical_to_steep)?,
+            support_vertical: load_optional_model(&self.support_vertical)?,
+            support_vertical_twist: load_optional_model(&self.support_vertical_twist)?,
+            support_barrel_roll: load_optional_model(&self.support_barrel_roll)?,
+            support_half_loop: load_optional_model(&self.support_half_loop)?,
+            support_quarter_loop: load_optional_model(&self.support_quarter_loop)?,
+            support_corkscrew: load_optional_model(&self.support_corkscrew)?,
+            support_zero_g_roll: load_optional_model(&self.support_zero_g_roll)?,
+            support_large_zero_g_roll: load_optional_model(&self.support_large_zero_g_roll)?,
+        })
+    }
 }
 
 #[derive(Debug, serde::Deserialize)]
@@ -149,8 +160,7 @@ pub struct Track {
     #[expect(unused)]
     #[serde(default)]
     pub pivot: f32,
-    #[expect(unused)]
-    pub models: Models,
+    pub models: Models<std::path::PathBuf>,
 }
 
 #[derive(Debug, serde::Deserialize)]
@@ -187,7 +197,6 @@ pub struct Light {
 
 #[derive(Debug, serde::Deserialize)]
 pub struct Desc {
-    #[expect(unused)]
     pub tracks: Vec<Track>,
     #[expect(unused)]
     pub offsets: Option<Offsets>,
