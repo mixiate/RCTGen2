afe23e5ee45d1118a265d08ef6b034b855419d8a
mix <mixiate@proton.me>
2026-07-20 00:09:17 +0100

    rctgen2: Position track sprite using image offset

--- renderer/src/framebuffer.rs
+++ renderer/src/framebuffer.rs
@@ -86,7 +86,7 @@ impl Framebuffer {
         )
     }
 
-    pub fn to_image(&self) -> crate::image::Image {
+    pub fn to_image(&self) -> (crate::image::Image, glam::IVec2) {
         let pixels = self
             .buffer
             .iter()
@@ -99,7 +99,10 @@ impl Framebuffer {
                 }
             })
             .collect::<Vec<u8>>();
-        crate::image::Image::from_raw(self.width, self.height, pixels)
+        (
+            crate::image::Image::from_raw(self.width, self.height, pixels),
+            self.offset(0, 0),
+        )
     }
 
     fn into_indexed_image_inner(mut self, dither: bool, bounds: &[usize; 4]) -> crate::image::IndexedImage {
