f5d7b2d4070e46b27aae8eb58f82339192490f0f
mix <mixiate@proton.me>
2026-08-07 22:30:00 +0100

    rctgen2: Draw sprites with remappable colours

--- rctgen2/src/drawing.rs
+++ rctgen2/src/drawing.rs
@@ -4,12 +4,15 @@ use crate::render::TrackImage;
 use crate::sprites;
 use make_track::track_desc;
 use make_track::track_desc::TrackSectionSprites;
+use openrct2::colour::Colour;
 use renderer::image::{Image, IndexedImage};
 
 pub struct Options {
     pub indexed: bool,
     pub adjacent_track: bool,
     pub original_track: bool,
+    pub colour_1: Colour,
+    pub colour_2: Colour,
 }
 
 impl Default for Options {
@@ -18,6 +21,8 @@ impl Default for Options {
             indexed: true,
             adjacent_track: false,
             original_track: false,
+            colour_1: Colour::LightBlue,
+            colour_2: Colour::BrightPink,
         }
     }
 }
@@ -89,7 +94,7 @@ fn draw_image(buffer: &mut Image, image: &Image, coords: &[i16; 3]) {
     }
 }
 
-fn draw_indexed_image(buffer: &mut Image, image: &IndexedImage, coords: &[i16; 3]) {
+fn draw_indexed_image(buffer: &mut Image, image: &IndexedImage, coords: &[i16; 3], colour_1: Colour, colour_2: Colour) {
     let (dest_x, dest_y, src_x, src_y, width, height) = clip_image_to_buffer(
         buffer.width(),
         buffer.height(),
@@ -102,6 +107,13 @@ fn draw_indexed_image(buffer: &mut Image, image: &IndexedImage, coords: &[i16; 3
     for y in 0..height {
         for x in 0..width {
             let pixel = image.get_pixel((src_x + x) as usize, (src_y + y) as usize);
+            let pixel = if (243..=254).contains(&pixel) {
+                openrct2::colour::COLOUR_RAMPS[colour_1 as usize][usize::from(pixel) - 243]
+            } else if (202..=213).contains(&pixel) {
+                openrct2::colour::COLOUR_RAMPS[colour_2 as usize][usize::from(pixel) - 202]
+            } else {
+                pixel
+            };
             if pixel != 0 {
                 let [r, g, b] = renderer::palette::PALETTE[usize::from(pixel)];
                 buffer.set_pixel((dest_x + x) as usize, (dest_y + y) as usize, [r, g, b, 255]);
@@ -125,6 +137,7 @@ fn compare_coords(coords: &[i16; 3], draw_order: DrawOrder) -> bool {
 
 fn draw_adjacent_track_section(
     track_image: &TrackImage,
+    options: &Options,
     sprites: &mut sprites::Sprites,
     adjacent_sections: &[TrackSectionWithSprites],
     draw_order: DrawOrder,
@@ -148,7 +161,7 @@ fn draw_adjacent_track_section(
                     continue;
                 }
                 if let Some(sprite) = sprites.get(sprite.index) {
-                    draw_indexed_image(buffer, sprite, &coords);
+                    draw_indexed_image(buffer, sprite, &coords, options.colour_1, options.colour_2);
                 }
             }
         }
@@ -160,6 +173,7 @@ fn draw_original_track_section(
     rotation: usize,
     sprites: &mut sprites::Sprites,
     track_sprites: &TrackSectionSprites,
+    options: &Options,
     buffer: &mut Image,
 ) {
     for (tile_coords, track_sprites) in track_section.tiles.iter().zip(track_sprites.iter()) {
@@ -167,7 +181,7 @@ fn draw_original_track_section(
         for sprite in &track_sprites[rotation] {
             let coords = add_coords(&coords, &sprite.offset);
             if let Some(sprite) = sprites.get(sprite.index) {
-                draw_indexed_image(buffer, sprite, &coords);
+                draw_indexed_image(buffer, sprite, &coords, options.colour_1, options.colour_2);
             }
         }
     }
@@ -187,7 +201,14 @@ fn draw_with_adjacent_sprites(
         track_desc_sprites,
     );
 
-    draw_adjacent_track_section(track_image, sprites, &adjacent_sections, DrawOrder::Before, buffer);
+    draw_adjacent_track_section(
+        track_image,
+        options,
+        sprites,
+        &adjacent_sections,
+        DrawOrder::Before,
+        buffer,
+    );
     if options.original_track
         && let Some(track_sprites) = track_desc_sprites.get(track_image.track_section.name)
     {
@@ -196,14 +217,28 @@ fn draw_with_adjacent_sprites(
             track_image.rotation,
             sprites,
             track_sprites,
+            options,
             buffer,
         );
     } else if options.indexed {
-        draw_indexed_image(buffer, &track_image.images.indexed, &[0; 3]);
+        draw_indexed_image(
+            buffer,
+            &track_image.images.indexed,
+            &[0; 3],
+            options.colour_1,
+            options.colour_2,
+        );
     } else {
         draw_image(buffer, &track_image.images.unindexed, &[0; 3]);
     }
-    draw_adjacent_track_section(track_image, sprites, &adjacent_sections, DrawOrder::After, buffer);
+    draw_adjacent_track_section(
+        track_image,
+        options,
+        sprites,
+        &adjacent_sections,
+        DrawOrder::After,
+        buffer,
+    );
 }
 
 pub fn draw(
@@ -234,10 +269,17 @@ pub fn draw(
             track_image.rotation,
             sprites,
             track_sprites,
+            options,
             buffer,
         );
     } else if options.indexed {
-        draw_indexed_image(buffer, &track_image.images.indexed, &[0; 3]);
+        draw_indexed_image(
+            buffer,
+            &track_image.images.indexed,
+            &[0; 3],
+            options.colour_1,
+            options.colour_2,
+        );
     } else {
         draw_image(buffer, &track_image.images.unindexed, &[0; 3]);
     }
