d01aec042636e8169f8508fb2a38ea02c25842c8
mix <mixiate@proton.me>
2026-08-09 23:52:44 +0100

    rctgen2: Add metal supports to track desc and draw them

--- rctgen2/src/drawing/mod.rs
+++ rctgen2/src/drawing/mod.rs
@@ -1,4 +1,5 @@
 mod blit;
+mod supports_metal;
 mod track;
 
 pub use track::draw;
@@ -7,8 +8,10 @@ pub struct Options {
     pub indexed: bool,
     pub adjacent_track: bool,
     pub original_track: bool,
+    pub supports: bool,
     pub colour_1: openrct2::colour::Colour,
     pub colour_2: openrct2::colour::Colour,
+    pub colour_3: openrct2::colour::Colour,
 }
 
 impl Default for Options {
@@ -17,8 +20,24 @@ impl Default for Options {
             indexed: true,
             adjacent_track: false,
             original_track: false,
+            supports: false,
             colour_1: openrct2::colour::Colour::LightBlue,
             colour_2: openrct2::colour::Colour::BrightPink,
+            colour_3: openrct2::colour::Colour::Yellow,
         }
+    }
+}
+
+pub fn add_coords(a: &[i16; 3], b: &[i16; 3]) -> [i16; 3] {
+    [a[0] + b[0], a[1] + b[1], a[2] + b[2]]
+}
+
+pub fn rotate_coords(coordinates: &[i16; 3], rotation: usize) -> [i16; 3] {
+    let [x, y, z] = *coordinates;
+    match rotation {
+        1 => [y, -x, z],
+        2 => [-x, -y, z],
+        3 => [-y, x, z],
+        _ => [x, y, z],
     }
 }
