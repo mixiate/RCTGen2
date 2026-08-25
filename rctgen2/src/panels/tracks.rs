4c2fb5a8f844a25dd89ac6ca4d77b42cc15db8c1
mix <mixiate@proton.me>
2026-08-25 01:38:53 +0100

    rctgen2: Add main track models to tracks side panel

--- rctgen2/src/panels/tracks.rs
+++ rctgen2/src/panels/tracks.rs
@@ -1,5 +1,6 @@
 use crate::widgets;
 use eframe::egui;
+use relative_path::RelativePathBuf;
 
 fn length_widgets(ui: &mut egui::Ui, value: &mut Option<f32>) -> bool {
     let mut changed = false;
@@ -24,7 +25,172 @@ fn length_widgets(ui: &mut egui::Ui, value: &mut Option<f32>) -> bool {
     changed
 }
 
-pub fn tracks_panel(tracks: &mut [make_track::track_desc::Track], ui: &mut egui::Ui) -> bool {
+fn model_file_dialog(directory: &std::path::Path) -> anyhow::Result<Option<RelativePathBuf>> {
+    use anyhow::Context as _;
+
+    let result = rfd::FileDialog::new().add_filter("obj", &["obj"]).set_directory(directory).pick_file();
+    if let Some(file_path) = result {
+        let file_path = file_path.strip_prefix(directory).context("Model file must be in the track directory.")?;
+        let file_path = RelativePathBuf::from_path(file_path)?;
+        Ok(Some(file_path))
+    } else {
+        Ok(None)
+    }
+}
+
+fn model_widgets(
+    ui: &mut egui::Ui,
+    label: &str,
+    value: &mut RelativePathBuf,
+    directory: &std::path::Path,
+    errors: &mut Vec<String>,
+    changed: &mut bool,
+) {
+    ui.with_layout(egui::Layout::right_to_left(egui::Align::Center), |ui| {
+        ui.label(label);
+    });
+    if ui.add(egui::Button::new("📁")).clicked() {
+        match model_file_dialog(directory) {
+            Ok(Some(file_path)) => {
+                *value = file_path;
+                *changed = true;
+            }
+            Err(error) => {
+                errors.push(error.to_string());
+            }
+            _ => {}
+        }
+    }
+    ui.label(value.as_str());
+}
+
+fn optional_model_widgets(
+    ui: &mut egui::Ui,
+    label: &str,
+    value: &mut Option<RelativePathBuf>,
+    directory: &std::path::Path,
+    errors: &mut Vec<String>,
+    changed: &mut bool,
+) {
+    ui.with_layout(egui::Layout::right_to_left(egui::Align::Center), |ui| {
+        ui.label(label);
+    });
+    if ui.add(egui::Button::new("📁")).clicked() {
+        match model_file_dialog(directory) {
+            Ok(Some(file_path)) => {
+                *value = Some(file_path);
+                *changed = true;
+            }
+            Err(error) => {
+                errors.push(error.to_string());
+            }
+            _ => {}
+        }
+    }
+    let mut removed = false;
+    if let Some(value) = value {
+        ui.label(value.as_str());
+        if widgets::buttons::remove_button(ui) {
+            removed = true;
+            *changed = true;
+        }
+    }
+    if removed {
+        *value = None;
+    }
+}
+
+fn models_collapsible(
+    ui: &mut egui::Ui,
+    models: &mut make_track::track_desc::Models<RelativePathBuf>,
+    dir: &std::path::Path,
+    errors: &mut Vec<String>,
+) -> bool {
+    let mut changed = false;
+    egui::Grid::new("Track models grid").min_col_width(0.0).show(ui, |ui| {
+        model_widgets(ui, "Track", &mut models.track, dir, errors, &mut changed);
+        ui.end_row();
+
+        model_widgets(ui, "Mask", &mut models.mask, dir, errors, &mut changed);
+        ui.end_row();
+
+        optional_model_widgets(ui, "Tie", &mut models.tie, dir, errors, &mut changed);
+        ui.end_row();
+
+        optional_model_widgets(ui, "Track Alt", &mut models.track_alt, dir, errors, &mut changed);
+        ui.end_row();
+
+        optional_model_widgets(ui, "Track Tie", &mut models.track_tie, dir, errors, &mut changed);
+        ui.end_row();
+
+        optional_model_widgets(ui, "Support Base", &mut models.support_base, dir, errors, &mut changed);
+        ui.end_row();
+
+        optional_model_widgets(ui, "Support Flat", &mut models.support_flat, dir, errors, &mut changed);
+        ui.end_row();
+
+        optional_model_widgets(
+            ui,
+            "Support Bank 1/6",
+            &mut models.support_bank_sixth,
+            dir,
+            errors,
+            &mut changed,
+        );
+        ui.end_row();
+
+        optional_model_widgets(
+            ui,
+            "Support Bank 1/3",
+            &mut models.support_bank_third,
+            dir,
+            errors,
+            &mut changed,
+        );
+        ui.end_row();
+
+        optional_model_widgets(
+            ui,
+            "Support Bank 1/2",
+            &mut models.support_bank_half,
+            dir,
+            errors,
+            &mut changed,
+        );
+        ui.end_row();
+
+        optional_model_widgets(
+            ui,
+            "Support Bank 2/3",
+            &mut models.support_bank_two_thirds,
+            dir,
+            errors,
+            &mut changed,
+        );
+        ui.end_row();
+
+        optional_model_widgets(
+            ui,
+            "Support Bank 5/6",
+            &mut models.support_bank_five_sixths,
+            dir,
+            errors,
+            &mut changed,
+        );
+        ui.end_row();
+
+        optional_model_widgets(ui, "Support Bank", &mut models.support_bank, dir, errors, &mut changed);
+        ui.end_row();
+    });
+    changed
+}
+
+pub fn tracks_panel(
+    tracks: &mut [make_track::track_desc::Track],
+    directory: &std::path::Path,
+    errors: &mut Vec<String>,
+    ui: &mut egui::Ui,
+) -> bool {
     let mut changed = false;
     egui::Panel::right("Tracks side panel").resizable(false).show(ui, |ui| {
         ui.style_mut().spacing.scroll = egui::style::ScrollStyle::solid();
@@ -138,7 +304,13 @@ pub fn tracks_panel(tracks: &mut [make_track::track_desc::Track], ui: &mut egui:
                         ui.end_row();
                     });
 
-                    egui::CollapsingHeader::new("Sections").id_salt(index).show(ui, |ui| {
+                    egui::CollapsingHeader::new("Models").id_salt(index + 512).show(ui, |ui| {
+                        if models_collapsible(ui, &mut track.models, directory, errors) {
+                            changed = true;
+                        }
+                    });
+
+                    egui::CollapsingHeader::new("Sections").id_salt(index + 1024).show(ui, |ui| {
                         use strum::IntoEnumIterator as _;
                         for group in make_track::track_desc::TrackGroup::iter() {
                             let mut enabled = track.sections.contains(&group);
