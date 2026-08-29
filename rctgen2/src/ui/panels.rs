fd3b01a65dfdb2c9f1c201d671330de353dff06e
mix <mixiate@proton.me>
2026-08-29 17:30:37 +0100

    rctgen2: Move side panel drawing to ui::panels module

--- rctgen2/src/ui/panels.rs
+++ rctgen2/src/ui/panels.rs
@@ -5,7 +5,10 @@ pub mod render;
 pub mod sprites;
 pub mod tracks;
 
+use crate::app;
+use crate::ui;
 use eframe::egui;
+use make_track::track_sections::TrackSection;
 
 #[derive(Clone, Copy, PartialEq)]
 pub enum SidePanelTab {
@@ -51,4 +54,69 @@ pub fn side_panel_tabs(ui: &mut egui::Ui, selected_tab: &mut Option<SidePanelTab
             });
         });
     });
+}
+
+#[expect(clippy::too_many_arguments)]
+pub fn side_panel(
+    ui: &mut egui::Ui,
+    tab: SidePanelTab,
+    sprites_track_selection_modal: &mut ui::modals::TrackSectionSelectionModal,
+    track_desc_path: &mut Option<std::path::PathBuf>,
+    track_desc: &mut make_track::track_desc::Desc,
+    current_track_section: &TrackSection,
+    rotation: usize,
+    changes: &mut app::Changes,
+    errors: &mut Vec<String>,
+) {
+    match tab {
+        SidePanelTab::Tracks => {
+            if let Some(path) = &track_desc_path
+                && let Some(directory) = path.parent()
+            {
+                tracks::tracks_panel(
+                    &mut track_desc.tracks,
+                    directory,
+                    errors,
+                    current_track_section,
+                    changes,
+                    ui,
+                );
+            }
+        }
+        SidePanelTab::Lights => {
+            let changed = lights::lights_panel(&mut track_desc.lights, ui);
+            if changed {
+                changes.render = true;
+            }
+        }
+        SidePanelTab::Offsets => {
+            let changed = offsets::offsets_panel(&mut track_desc.offsets, ui);
+            if changed {
+                changes.offsets = true;
+            }
+        }
+        SidePanelTab::MetalSupports => {
+            let changed = metal_supports::metal_supports_panel(
+                &mut track_desc.metal_supports,
+                rotation,
+                current_track_section,
+                ui,
+            );
+            if changed {
+                changes.redraw = true;
+            }
+        }
+        SidePanelTab::Render => {
+            let changed = render::render_panel(track_desc, ui);
+            if changed {
+                changes.render = true;
+            }
+        }
+        SidePanelTab::Sprites => {
+            let changed = sprites::sprites_panel(&mut track_desc.original_sprites, sprites_track_selection_modal, ui);
+            if changed {
+                changes.redraw = true;
+            }
+        }
+    }
 }
