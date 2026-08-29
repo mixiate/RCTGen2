887ddfe80446a6f4d2055e6083515e757e858847
mix <mixiate@proton.me>
2026-08-29 15:08:14 +0100

    rctgen2: Add list of recent files to settings and sub menu to load them

--- rctgen2/src/settings.rs
+++ rctgen2/src/settings.rs
@@ -1,9 +1,28 @@
 use eframe::egui;
+use std::collections::VecDeque;
 
 #[derive(Default, serde::Deserialize, serde::Serialize)]
 pub struct Settings {
     #[serde(skip_serializing_if = "Option::is_none")]
     pub g1_dat_path: Option<std::path::PathBuf>,
+    #[serde(default, skip_serializing_if = "VecDeque::is_empty")]
+    recent_files: VecDeque<std::path::PathBuf>,
+}
+
+impl Settings {
+    pub fn recent_files(&self) -> &VecDeque<std::path::PathBuf> {
+        &self.recent_files
+    }
+
+    pub fn add_recent_file(&mut self, file_path: &std::path::Path) {
+        self.recent_files.retain(|x| *x != file_path);
+        self.recent_files.push_front(file_path.to_path_buf());
+        self.recent_files.truncate(50);
+    }
+
+    pub fn clear_recent_files(&mut self) {
+        self.recent_files.clear();
+    }
 }
 
 pub struct AppSettings {
