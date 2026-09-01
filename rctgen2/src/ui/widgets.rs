3f81ef7a378f2746c8ffc3a9c7a0cc8f1df43ec4
mix <mixiate@proton.me>
2026-09-01 13:51:29 +0100

    rctgen2: Remove widgets::tile_index_label and use format! instead

--- rctgen2/src/ui/widgets.rs
+++ rctgen2/src/ui/widgets.rs
@@ -1,24 +1,5 @@
-use eframe::egui;
-
 pub mod buttons;
 pub mod colour_picker;
 mod drag_value_spin;
 
 pub use drag_value_spin::DragValueSpin;
-
-pub fn tile_index_label(ui: &mut egui::Ui, tile_index: usize) {
-    let tile_label = match tile_index {
-        0 => "0:",
-        1 => "1:",
-        2 => "2:",
-        3 => "3:",
-        4 => "4:",
-        5 => "5:",
-        6 => "6:",
-        7 => "7:",
-        8 => "8:",
-        9 => "9:",
-        _ => "",
-    };
-    ui.label(tile_label);
-}
